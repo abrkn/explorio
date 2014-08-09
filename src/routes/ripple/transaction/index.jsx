9277179c70b2d8d775454d2c26c80984ed032ac4
Andreas Brekken <a@abrkn.com>
2014-08-09 18:08:37 +0200

    Add more transaction info

--- src/routes/ripple/transaction/index.jsx
+++ src/routes/ripple/transaction/index.jsx
@@ -44,6 +44,46 @@ var Transaction = React.createClass({
             <th>Account</th>
             <td>{tx && <AccountLink id={tx.Account} />}</td>
           </tr>
+          <tr>
+            <th>Type</th>
+            <td>{tx && tx.TransactionType}</td>
+          </tr>
+          <tr>
+            <th>Ledger</th>
+            <td>{tx && tx.inLedger}</td>
+          </tr>
+          <tr>
+            <th>Fee</th>
+            <td>{tx && formatters.formatAmount(tx.Fee, constants.networks.RIPPLE)}</td>
+          </tr>
+          {tx && tx.TakerPays && <tr>
+            <th>Taker pays</th>
+            <td>{formatters.formatAmount(tx.TakerPays, constants.networks.RIPPLE)}</td>
+          </tr>}
+          {tx && tx.TakerGets && <tr>
+            <th>Taker gets</th>
+            <td>{formatters.formatAmount(tx.TakerGets, constants.networks.RIPPLE)}</td>
+          </tr>}
+          {tx && tx.Destination && <tr>
+            <th>Destination</th>
+            <td><AccountLink id={tx.Destination} /></td>
+          </tr>}
+          {tx && tx.DestinationTag !== undefined && <tr>
+            <th>Destination tag</th>
+            <td>{tx.DestinationTag}</td>
+          </tr>}
+          {tx && tx.SourceTag !== undefined && <tr>
+            <th>Source tag</th>
+            <td>{tx.SourceTag}</td>
+          </tr>}
+          {tx && tx.Amount && <tr>
+            <th>Amount</th>
+            <td>{formatters.formatAmount(tx.Amount, constants.networks.RIPPLE)}</td>
+          </tr>}
+          {tx && tx.Flags !== undefined && <tr>
+            <th>Flags</th>
+            <td>{tx.Flags}</td>
+          </tr>}
         </tbody>
       </table>
     </div>
