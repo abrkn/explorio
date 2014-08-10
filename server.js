21b43a04bd34a611ed21d1d9cb467d8cfaa53554
Andreas Brekken <a@abrkn.com>
2014-08-10 10:33:26 +0200

    Force https

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ var express = require('express')
 var path = require('path')
 var compression = require('compression')
 
+function forceHttps(req, res, next) {
+    if (req.headers['x-forwarded-proto'] == 'https') return next()
+    res.redirect('https://explor.io' + req.url)
+}
+
 var app = express()
 
 app.set('x-powered-by', false)
 app.set('trust proxy', true)
 app.set('case sensitive routing', true)
+
+if (process.env.NODE_ENV == 'production') {
+    app.use(forceHttps)
+}
 
 app.use(compression())
 
